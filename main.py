4a398d40b509b61888cb3f9a395b6d4598db5dc5
Joshua Rea <josh.rea@gmail.com>
2022-04-05 17:01:24 -0700

    add block function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from npc_combat import get_attack_value
+from npc_combat import get_attack_value, get_blk_value
 
 
 atk = get_attack_value()
+blk = get_blk_value()
 
 print(atk)
+print(blk)
--- npc_combat.py
+++ npc_combat.py
@@ -2,10 +2,19 @@ import random
 
 
 def get_attack_value():
-  atk = random.randint(1, 6)
-  if atk == 1 or atk == 6:
-    return "Attack High"
-  elif atk == 2 or atk == 5:
-    return "Attack Middle"
-  elif atk == 3 or atk == 4:
+   atk = random.randint(1, 6)
+   if atk == 1 or atk == 6:
+     return "Attack High"
+   elif atk == 2 or atk == 5:
+     return "Attack Middle"
+   elif atk == 3 or atk == 4:
     return "Attack Low"
+
+def get_blk_value():
+    blk = random.randint(1, 6)
+    if blk == 1 or blk == 6:
+        return "Defend High"
+    elif blk == 2 or blk == 5:
+        return "Defend Middle"
+    elif blk == 3 or blk == 4:
+        return "Defend Low"
