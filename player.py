8676608560bfceca7c4d3a9f88c96765f191e4f3
Jed Duffey <jedduffey@gmail.com>
2022-05-06 10:50:32 -0400

    Add additional initial attributes to Player class

--- player.py
+++ player.py
@@ -1,4 +1,22 @@
+INITIAL_HP = 5
+INITIAL_COMBAT_STONES = 2
+INITIAL_REPUTATION_RANK = 1
+INITIAL_POWER = 0
+INITIAL_AGILITY = 0
+INITIAL_STAMINA = 0
+INITIAL_CHI = 0
+INITIAL_WIT = 0
+
+
+
 class Player:
-    def __init__(self, initial_hp, town):
-        self.hp = initial_hp
+    def __init__(self, town):
         self.town = town
+        self.hp = INITIAL_HP
+        self.combat_stones = INITIAL_COMBAT_STONES
+        self.reputation_rank = INITIAL_REPUTATION_RANK
+        self.pow = INITIAL_POWER
+        self.agi = INITIAL_AGILITY
+        self.sta = INITIAL_STAMINA
+        self.chi = INITIAL_CHI
+        self.wit = INITIAL_WIT
--- player_test.py
+++ player_test.py
@@ -5,19 +5,37 @@ import uuid
 import player
 
 
-def test_when_constructed_then_hp_is_initial_hp():
-    initial_hp = random.randint(1, sys.maxsize)
-    town = uuid.uuid4()
+def test_when_constructed_then_hp_is_5():
+    sut = player.Player(uuid.uuid4())
 
-    sut = player.Player(initial_hp, town)
+    assert sut.hp == 5
 
-    assert sut.hp == initial_hp
+
+def test_when_constructed_then_combat_stones_is_2():
+    sut = player.Player(uuid.uuid4())
+
+    assert sut.combat_stones == 2
+
+
+def test_when_constructed_then_reputation_rank_is_1():
+    sut = player.Player(uuid.uuid4())
+
+    assert sut.reputation_rank == 1
+
+
+def test_when_constructed_then_stats_are_0():
+    sut = player.Player(uuid.uuid4())
+
+    assert sut.pow == 0
+    assert sut.agi == 0
+    assert sut.sta == 0
+    assert sut.chi == 0
+    assert sut.wit == 0
 
 
 def test_when_constructed_then_town_is_ctor_value():
-    initial_hp = random.randint(1, sys.maxsize)
     town = uuid.uuid4()
 
-    sut = player.Player(initial_hp, town)
+    sut = player.Player(town)
 
     assert sut.town == town
