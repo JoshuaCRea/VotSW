1e25e5729ff0e85efcbd75983f63ab32baf2f33a
experimentaldigitalresearch <103909859+experimentaldigitalresearch@users.noreply.github.com>
2022-05-10 20:05:09 -0400

    Create Player class (#7)
    * Create Player class with tests; implement Player class in main
    
    * refactor Game Over message logic
    
    * Address PR comment: fix condition in Game Over section

---
+++ player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self):
+        self.hp = 5
+        self.is_alive = True
+
+    def receive_damage(self, damage_points):
+        self.hp = self.hp - damage_points
+        if self.hp <= 0:
+            self.is_alive = False
